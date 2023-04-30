dc4ac68efee07bf102ff1a87d7abaa9fa82cc29a
Mohamed Khaled Samir <mouhamed_khaled@yahoo.com>
2023-05-01 01:19:27 +0300

    01_challenge_solution

---
+++ operators_challenge.js
@@ -0,0 +1,10 @@
+let d = "-100";
+let e = "20";
+let f = 30;
+let g = true;
+
+// Only Use Variables Value
+// Do Not Use Variable Twice
+
+console.log(-d * +e); // 2000
+console.log(-d + ++e * ++g + ++f); // 173
