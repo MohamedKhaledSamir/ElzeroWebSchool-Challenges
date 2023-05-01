76cca354eae85dee4481879674a3489873b975a8
Mohamed Khaled Samir <mouhamed_khaled@yahoo.com>
2023-05-01 08:32:33 +0300

    convert to arrow function

--- Arrow_Function_Challenge.js
+++ Arrow_Function_Challenge.js
@@ -6,11 +6,6 @@
 // [2] Convert To Arrow Function
 // [3] Print The Output [Arguments May Change]
 
-let names = function (...names) {
-  // Parameter ?
-
-  return `String [${names.join("],[")}] => Done !`;
-};
-
+let names = (...names) => `String [${names.join("],[")}] => Done !`;
 console.log(names("Osama", "Mohamed", "Ali", "Ibrahim"));
 // String [Osama], [Mohamed], [Ali], [Ibrahim] => Done !
