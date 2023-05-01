80b6bd60650065acb3ebd2b341d750352a3570d4
Mohamed Khaled Samir <mouhamed_khaled@yahoo.com>
2023-05-01 08:30:25 +0300

    08_Challenge

---
+++ Arrow_Function_Challenge.js
@@ -0,0 +1,16 @@
+/*
+  Function Arrow Challenges
+*/
+
+// [1] One Statement In Function
+// [2] Convert To Arrow Function
+// [3] Print The Output [Arguments May Change]
+
+let names = function (...names) {
+  // Parameter ?
+
+  return `String [${names.join("],[")}] => Done !`;
+};
+
+console.log(names("Osama", "Mohamed", "Ali", "Ibrahim"));
+// String [Osama], [Mohamed], [Ali], [Ibrahim] => Done !
